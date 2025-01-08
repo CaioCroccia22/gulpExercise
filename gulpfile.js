c5c30edee9660fba58901b3cdde34f71b19e1338
caiocroccia22 <caiocroccia22@gmail.com>
2025-01-08 19:30:07 -0300

    feat: add compressedJS function to compress JS files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
 
 function convertSass(){
     return gulp.src('./source/styles/*.scss')
@@ -9,4 +10,28 @@ function convertSass(){
    
 }
 
+function compressedImg() {
+    return gulp.src('./source/img/*')
+        .pipe(imagemin([
+            imagemin.mozjpeg({ quality: 75, progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+                plugins: [
+                    { removeViewBox: false },
+                    { cleanupIDs: false }
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('./build/images/'));
+}
+
+function compressedJS(){
+    return gulp.src('./source/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js/'))
+}
+
+
 exports.convertSass = convertSass;
+exports.imagemin = compressedImg;
+exports.jsTask = compressedJS;
